4499f0dca64ee9fe4eb3926ed13805c34a543e1f
Randy Grayson <rgrayson@avifoodsystems.com>
2017-02-17 18:01:15 +0000

    20170217

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,10 @@ require File.expand_path('../application', __FILE__)
 
 
 #Load env_settings (ENV) variables....
-#env_settings is part of .gitignore
+#env_settings.rb file is part of .gitignore for security
 file1 = File.join(Rails.root, 'config', 'env_settings.rb')
 load(file1) if File.exists?(file1)
 
-#ENV['DB_USERNAME']='aviuser'
-#ENV['DB_PASSWORD']='avitest'
 
 # Initialize the Rails application.
 Rails.application.initialize!
