a88c1cb268c9eed23d9cd71197c3176e190db159
Randy Grayson <rgrayson@avifoodsystems.com>
2017-02-17 17:34:57 +0000

    20170217

--- config/env_settings.rb
+++ config/env_settings.rb
@@ -1,7 +1,10 @@
 #Used for account settings, passwords, api keys/values etc.
 #Sets the following environment variables
+
+#******************************************************************
 #Be sure to add this file to .gitignore so it will NOT
 #be pushed to Github (for security)
+#******************************************************************
 
 if Rails.env.production?
    ENV['DB_USERNAME']='aviuser'
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 
 #Load env_settings (ENV) variables....
+#env_settings is part of .gitignore
 file1 = File.join(Rails.root, 'config', 'env_settings.rb')
 load(file1) if File.exists?(file1)
 
