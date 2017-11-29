520ae07c8a333bb7571be531373eeba44b157dc8
Randy Grayson <rgrayson@avifoodsystems.com>
2017-11-29 16:37:28 +0000

    20171129

---
+++ db/migrate/20171129163200_tbl_delete_20171129.rb
@@ -0,0 +1,5 @@
+class TblDelete20171129 < ActiveRecord::Migration
+  def change
+    drop_table :app_configs
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122173908) do
+ActiveRecord::Schema.define(version: 20171129163200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,6 @@ ActiveRecord::Schema.define(version: 20171122173908) do
     t.string   "state"
     t.string   "zip"
     t.string   "produced_by"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "app_configs", force: :cascade do |t|
-    t.datetime "last_update"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
