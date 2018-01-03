87678914b8ad5fe6a42cc1c6ac4284fc7792282b
Malachai Frazier The Great <malachaifrazier@gmail.com>
2018-01-03 13:43:28 -0500

    startup_controller and work_print_label_controller re-formatted

--- app/controllers/startup_controller.rb
+++ app/controllers/startup_controller.rb
@@ -1,63 +1,47 @@
 class StartupController < ApplicationController
 
-def index
-   #perform any startup maintenance
-   #in the future all tables will be updated nightly
-   #by some method from our SQL tables
-   
-   #delete any session data
-   session[:u]=nil
-   session[:lt]=nil
-   
-  
-end    
+  def index
+    #perform any startup maintenance
+    #in the future all tables will be updated nightly
+    #by some method from our SQL tables
+    #delete any session data
+    session[:u]  = nil
+    session[:lt] = nil
+  end
 
+  def login
+    #set session variables
+    session[:u] = params[:username]
+    session[:category_def] = 'All'
 
-def login
-    
-   #set session variables
-   session[:u]=params[:username]   
-   session[:category_def]='All'
-  
-   #capture params    
-    u=params[:username]
-    p=params[:password]
-    
-   
-   #check for missing controls/values
-      @e=0
-      @msg=""
-      if u==""
-          @e=1
-          @msg="Invalid/Missing Username."
-      elsif p ==""
-          @e=1
-          @msg="Invalid/Missing Password."
-      end
-  
-     
-      if @e==1
-         #missing data,
-         #why do I have to pass instance variables?  They will not show up in view any other way?
-        redirect_to root_path(e: @e, msg: @msg,u: u,p: p)and return
-      end
-      
-  #validate login
-  
-   t=Appconfig.where("pk=1")
-   t.each do |tbl|
-     session[:last_update]=tbl.last_update
-   end
-   
-  
-  #direct to user index
-     redirect_to recipe_index_path and return
-  
-  
-end    
+    #capture params
+    u = params[:username]
+    p = params[:password]
 
+    #check for missing controls/values
+    @e   = 0
+    @msg = ''
+    if u == ''
+      @e = 1
+      @msg = 'Invalid/Missing Username.'
+    elsif p == ''
+      @e = 1
+      @msg = 'Invalid/Missing Password.'
+    end
 
+    if @e == 1
+      #missing data,
+      #why do I have to pass instance variables?  They will not show up in view any other way?
+      redirect_to root_path(e: @e, msg: @msg, u: u, p: p)and return
+    end
 
+    #validate login
+    t = Appconfig.where('pk=1')
+    t.each do |tbl|
+      session[:last_update]=tbl.last_update
+    end
 
-
+    #direct to user index
+    redirect_to recipe_index_path and return
+  end
 end
--- app/controllers/work_print_label_controller.rb
+++ app/controllers/work_print_label_controller.rb
@@ -1,14 +1,11 @@
 class WorkPrintLabelController < ApplicationController
 
-def update
-
+  def update
     #called from show1/"Print labels".  Will will take data from
     #and insert into work table used for printing, then envoke "print"
-    
-    
     #save all to work table
-    @t_work=WorkPrintLabel.all()
-    @t_work.each do |tbl| 
+    @t_work = WorkPrintLabel.all()
+    @t_work.each do |tbl|
       tbl.copies = params[:copies]
       tbl.label_name2 = params[:label_name2]
       tbl.sel_prc2 = params[:sel_prc2]
@@ -26,36 +23,30 @@ def update
       tbl.div2=params[:div2]
       tbl.acnt_name2=params[:acnt_name2]
         # #get accnt dependant info
-        t2=Accntlist.where("acnt_name=?",params[:acnt_name2])
+      t2 = Accntlist.where("acnt_name=?",params[:acnt_name2])
         if t2.any?
-           t2.each do |tbl2|
-            tbl.produced_by2=tbl2.produced_by  
+          t2.each do |tbl2|
+            tbl.produced_by2=tbl2.produced_by
             tbl.address_full2= tbl2.address + ", " + tbl2.city + " " + tbl2.state + " " + tbl2.zip
-           end
-        else  
-            tbl.produced_by2='AVI FOODSYSTEMS INC'  
-            tbl.address_full2= '2590 ELM RD, WARREN OH 44483'
+          end
+        else
+          tbl.produced_by2='AVI FOODSYSTEMS INC'
+          tbl.address_full2= '2590 ELM RD, WARREN OH 44483'
         end
-        
-      r=tbl.valid?
-      if r==false
-          #errors detected
-          @t_recipe=Recipe.where("rid=?",params[:rid])
-          @t_error=tbl.errors.messages
-          render 'recipe/show1' and return
+
+      r = tbl.valid?
+      if r == false
+        #errors detected
+        @t_recipe=Recipe.where("rid=?",params[:rid])
+        @t_error=tbl.errors.messages
+        render 'recipe/show1' and return
       else
-          #ok to save
-          tbl.save
-          @t_error='saved'
-      end    
-      
+        #ok to save
+        tbl.save
+        @t_error='saved'
+      end
     end
-    
     render 'print_label'
-    
-end   
-
-
+  end
 
 end
-
